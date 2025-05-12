4d6406db4915bcd0b56aca25dc27d1c64a30d1c5
rpallin0705 <rpalomareslinares@gmail.com>
2025-05-12 18:58:26 +0200

    [ADD] Microservicio Producto registrado en eureka

--- microservice-eureka/src/main/java/com/microservice/eureka/MicroServiceEurekaApplication.java
+++ microservice-eureka/src/main/java/com/microservice/eureka/MicroServiceEurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// Configura la aplicación como un servidor eureka
 @EnableEurekaServer
 @SpringBootApplication
 public class MicroServiceEurekaApplication {
--- microservice-product/src/main/java/com/microservice/product/MicroserviceProductApplication.java
+++ microservice-product/src/main/java/com/microservice/product/MicroserviceProductApplication.java
@@ -2,7 +2,10 @@ package com.microservice.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// Esto indica que este microservicio va a ser un cliente de eureka [opciona]
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceProductApplication {
 
